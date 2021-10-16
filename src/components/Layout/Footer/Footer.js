21ac9cc5e78f6530ddba72b65f37bf10cc14251c
Luis De Freites <luisdefreites120@gmail.com>
2021-10-16 22:13:42 +0200

    finishing the footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,23 +1,34 @@
-import React from "react";
+import { contactInfo } from "../../../utils/contactInfo";
+import { useModal } from "../../../hooks/useModal";
+import { Modal } from "../../";
+
 import "./Footer.css";
 
 const Footer = () => {
+  const { contactMeInfo } = contactInfo;
+  const [modalInfo, handleOnClickModal] = useModal();
+
   return (
-    <footer>
-      <div className="container">
-        <div class="logo">
-          luisma<span>.dev</span>
+    <>
+      <Modal modalInfo={modalInfo} handleOnClosed={handleOnClickModal} />
+      <footer>
+        <div className="container">
+          <div class="logo">
+            luisma<span>.dev</span>
+          </div>
+          <ul>
+            <li>
+              <a href="#projects">Portfolio</a>
+            </li>
+            <li>
+              <a onClick={() => handleOnClickModal(contactMeInfo)}>
+                Contact me
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul>
-          <li>
-            <a href="#projects">Portfolio</a>
-          </li>
-          <li>
-            <a href="#">Contact me</a>
-          </li>
-        </ul>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 };
 
