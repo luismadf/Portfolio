b8dafd9b7a717243b80985ef416bc9e058045a7b
Luis De Freites <luisdefreites120@gmail.com>
2021-10-17 20:17:11 +0200

    finishing header

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Header from "./Header";
+import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Hero from "../Hero";
 
 const Layout = (props) => {
   return (
     <>
       <Header />
+      <Hero />
       {props.children}
       <Footer />
     </>
