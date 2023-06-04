9ee167ab905707971cfa84cf02a2a13fb31e4dc3
Luis De Freites <luis.defreites@ip-192-168-1-130.ec2.internal>
2023-06-04 12:46:00 +0200

    create new footer menu items

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,14 @@
 export * from "./project.utils";
+
+export const footerMenu = [
+  {
+    id: "1",
+    text: "GitHub",
+    url: "https://github.com/luismadf",
+  },
+  {
+    id: "2",
+    text: "LinkedIn",
+    url: "https://www.linkedin.com/in/luisdefreites/",
+  },
+];
