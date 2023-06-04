dc0e8e504ffa8fd2eff7a44f5adeae2ffc6ee1fe
Luis De Freites <luis.defreites@ip-192-168-1-130.ec2.internal>
2023-06-04 14:47:50 +0200

    fix delete contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { AboutMe, Contact, CV, Home, Project } from "pages";
+import { AboutMe, CV, Home, Project } from "pages";
 import { Layout } from "components";
 
 const App = () => {
@@ -8,7 +8,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:id" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/aboutme" element={<AboutMe />} />
         <Route path="/curriculum" element={<CV />} />
       </Routes>
