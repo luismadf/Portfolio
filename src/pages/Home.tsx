cf85183bced21445fbb7cd9a75886a11fc232962
Luis De Freites <luis.defreites@intelygenz.com>
2024-09-22 18:08:50 +0200

    feat: Update Hero section in Home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,28 @@
-import { ProjectList } from "components";
-import { projectList } from "utils";
+import { ProjectList } from 'components'
+import { projectList } from 'utils'
 
 const Home = () => {
   return (
     <main>
-      <section className="container mx-auto mt-12 mb-20">
-        <h2 className="text-4xl mb-4 font-light leading-snug">
-          Hola, Soy Luis. <br />
-          <span className="font-extrabold">Front End Developer</span> en
-          Intelygenz.
-        </h2>
-        <h5 className="mb-6 text-lg">
-          Me encanta solucionar problemas y entregar excelencia.
+      <section className='container mx-auto mt-12 mb-[84px] md:mt-[100px] md:mb-[200px] text-center flex flex-col gap-5'>
+        <h1 className='font-normal'>
+          Hi, I’m Luis 👋
+          <br />
+          <span className='block md:inline font-bold'>Frontend Developer</span> at Intelygenz.
+        </h1>
+
+        <h5>
+          I specialize in building apps using <span className='font-bold'>Next.js</span>,{' '}
+          <span className='font-bold'>React</span>, and{' '}
+          <span className='font-bold'>React Native</span> to create seamless user experiences.
         </h5>
       </section>
 
-      <section className="container mx-auto mb-24">
-        <h2 className="mb-12 text-4xl font-light">
-          Mis <span className="font-extrabold">proyectos</span>
-        </h2>
+      <section className='container mx-auto mb-24'>
         <ProjectList projects={projectList} />
       </section>
     </main>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
