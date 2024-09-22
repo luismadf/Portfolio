5b1d49cef06c0a4f554d93a124e68487f74e37c2
Luis De Freites <luis.defreites@intelygenz.com>
2024-09-22 17:50:42 +0200

    feat: Update index.css with new font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
-      padding: "2rem",
+      padding: '2rem',
+      screens: {
+        xl: '1200px',
+      },
     },
     extend: {
       gridTemplateRows: {
-        layout: "auto 1fr auto",
+        layout: 'auto 1fr auto',
       },
     },
   },
   plugins: [],
-};
+}
