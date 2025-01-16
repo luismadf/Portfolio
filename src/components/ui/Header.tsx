08e902e9c8326cacccb0a3dcbb1d4ebc87a92775
Luis De Freites <64321104+luismadf@users.noreply.github.com>
2025-01-16 21:37:41 +0100

    Feat/updating to v2.5 (#36)
    * Update README.md
    
    * create new footer menu items
    
    * create new folder types, and new footer types
    
    * create new Footer, use of new footer
    
    * fix styles based on new footer
    
    * delete contact page
    
    * fix delete contact page
    
    * Update react & react-dom to 18.2.0
    
    * feat: Add prettierrc file
    
    * feat: Delete AboutMe page & Menu component
    
    * feat: Update Logo component
    
    * feat: Create Button component
    
    * feat: Create Link component
    
    * feat: Update index.css with new font styles
    
    * chore: Delete AboutMe page
    
    * chore: Update Footer component
    
    * chore: Update Header component
    
    * feat: Update Hero section in Home screen
    
    * chore: Delete ProjectList component
    
    * feat: Update Project component
    
    * feat: update React rendering in index.tsx to use React 18 methods
    
    * chore: remove unused code from index.html and manifest.json
    
    * fix: add missing key to project stack map
    
    ---------
    
    Co-authored-by: Luis De Freites <luis.defreites@ip-192-168-1-130.ec2.internal>

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,51 +1,36 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { gsap } from "gsap";
-import { Logo, Menu, MobileMenu } from "components";
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { gsap } from 'gsap'
+import { Button, Logo } from 'components'
 
 const Header: React.FC = () => {
-  let navigate = useNavigate();
-
-  const handleOnNavigate = (path: string) => navigate(path);
-
-  const menuItems = [
-    {
-      id: "1",
-      text: "About me",
-      onClick: () => handleOnNavigate("/aboutme"),
-    },
-    {
-      id: "2",
-      text: "Descargar CV",
-      onClick: () => handleOnNavigate("/curriculum"),
-      cta: true,
-    },
-  ];
+  let navigate = useNavigate()
 
   useEffect(() => {
-    gsap.from(".logo-component", {
+    gsap.from('.logo-component', {
       opacity: 0,
       duration: 0.8,
       delay: 0.2,
-    });
-    gsap.from(".menu-item", {
+    })
+    gsap.from('.menu-item', {
       opacity: 0,
       duration: 0.8,
       delay: 0.2,
       y: 20,
       stagger: 0.2,
-    });
-  }, []);
+    })
+  }, [])
 
   return (
     <header>
-      <div className="container mx-auto py-6 flex justify-between items-center">
-        <Logo showPicture={true} />
-        <MobileMenu items={menuItems} />
-        <Menu items={menuItems} className="hidden md:flex" />
+      <div className='container mx-auto py-6 flex justify-between items-center'>
+        <Logo showLogoImage={true} />
+        <Button className='hidden sm:block' onClick={() => navigate('/curriculum')}>
+          Descargar CV
+        </Button>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
